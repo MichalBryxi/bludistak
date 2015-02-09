32f24a017cbea15308599c29451389fb8f79ce2f
Maniac <maniac1234@gmail.com>
2015-02-09 16:26:42 +0000

    bugik 32

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  
+  layout :determine_layout
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -15,4 +18,11 @@ class ApplicationController < ActionController::Base
       redirect_to finish_signup_path(current_user)
     end
   end
+  
+  private
+  def determine_layout
+    current_user ? "private" : "public"
+  end
+  
+  
 end
