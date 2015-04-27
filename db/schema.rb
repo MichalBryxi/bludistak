1f501e0b3b49969918269a93c11e4e403c6e752e
iff <ivanakozlova@gmail.com>
2015-04-27 16:11:13 +0000

    nova polozka obrazky do tabulky project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202180533) do
+ActiveRecord::Schema.define(version: 20150427150937) do
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150202180533) do
     t.boolean  "active",      default: true, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
   create_table "users", force: true do |t|
